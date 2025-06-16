206772a63468bce28b83c968f13385107392faee
krishnapavani11 <krishnapavani2002@gmail.com>
2025-06-16 22:54:43 +0530

    Renamed Student to User across the project initial changes

--- UserInfo/Program.cs
+++ UserInfo/Program.cs
@@ -11,23 +11,20 @@ namespace UserInfo
             FileOperations fileops = new FileOperations();
             string path = "C:\\Users\\pav\\source\\repos\\UserInfo\\Users.csv";
 
-            Console.WriteLine("Welcome To Student Details File");
-
-
-            // Initialize Serial Number based on existing data
+            Console.WriteLine("Welcome To User Details File");
             int SNo = 0;
 
             int iNumber;
 
             string Content()
             {
-                Console.WriteLine("Enter Student Name: ");
+                Console.Write("Enter user Name: ");
                 string sName = Console.ReadLine();
 
-                Console.WriteLine("Enter Student Age: ");
+                Console.Write("Enter user Age: ");
                 int iAge = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter Student Email: ");
+                Console.Write("Enter user Email: ");
                 string sEmail = Console.ReadLine();
 
                 SNo++; // increment for each student
@@ -38,8 +35,8 @@ namespace UserInfo
             {
                 Console.WriteLine("\nMain Menu");
                 Console.WriteLine("------------------------------");
-                Console.WriteLine("1. Add Student");
-                Console.WriteLine("2. List Of Students");
+                Console.WriteLine("1. Add User");
+                Console.WriteLine("2. List Of User");
                 Console.WriteLine("3. Exit");
                 Console.WriteLine("------------------------------");
                 Console.Write("Enter your Choice: ");
@@ -49,12 +46,12 @@ namespace UserInfo
                 {
                     case 1:
                         fileops.AppendContent(path, Content());
-                        Console.WriteLine("\nStudent Added Successfully.\n");
+                        Console.WriteLine("\nUser Added Successfully.\n");
                         break;
 
                     case 2:
                         string readContent = fileops.ReadContent(path);
-                        Console.WriteLine("\nList Of Students:\n" + readContent);
+                        Console.WriteLine("\nList Of User:\n" + readContent);
                         break;
 
                     case 3:
